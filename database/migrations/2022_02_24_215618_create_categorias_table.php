0c0e99893e861e21d552bbfc53cd06c7350e7949
Henrique Morozini Hupp <henriquehupp@hotmail.com>
2022-03-03 18:27:25 -0300

    Desafio Adapti V1

--- database/migrations/2022_02_24_215618_create_categorias_table.php
+++ database/migrations/2022_02_24_215618_create_categorias_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('categorias', function (Blueprint $table) {
+            //id, categoria
+            $table->id();
+            $table->timestamps();
+            $table->string('categoria');
         });
     }
 
@@ -22,8 +24,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('categorias');
     }
 };
--- database/migrations/2022_02_24_220702_create_produtos_table.php
+++ database/migrations/2022_02_24_220702_create_produtos_table.php
@@ -4,17 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome', 100);
+            $table->float('preço', 8, 2);
+            $table->string('descricao', 100);
+            $table->integer('quantidade');
+            $table->binary('imagem');
+            $table->string('categoria_id');
         });
+
+        //id, nome, preco, descricao, quantidade, imagem e categoria_id)
     }
 
     /**
@@ -22,8 +30,7 @@ return new class extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('produtos');
     }
 };
